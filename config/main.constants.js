f3373af9aca5ee1f3eebff744510841f95764e4b
LaFabbrica <marcio@lfdb.com.br>
2015-04-14 08:23:26 +0000

    Release 04-14-15

--- config/main.constants.js
+++ config/main.constants.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'https://zup-api-tim.ntxdev.com.br',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'11',flowsEnabled:'',mapDebug:''})
+.constant('ENV', {name:'production',apiEndpoint:'https://zup-api-tim.ntxdev.com.br',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'11',flowsEnabled:'',mapDebug:'',logoImgUrl:''})
 
 ;
